dc0c2e6663b84b63328f38daa0a23a6f98238b6e
danmor708 <101704185+danmor708@users.noreply.github.com>
2022-04-26 10:27:01 +0200

    Update EjercicioContarLetras.java

--- src/main/java/EjercicioContarLetras.java
+++ src/main/java/EjercicioContarLetras.java
@@ -1,9 +1,46 @@
-public class EjercicioContarLetras {
+/*public class EjercicioContarLetras {
     class Ejemplo1 {
         public static void Main(){
             String str = "";
             char[] arr;
         }
+
+    }
+}
+*/
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class EjercicioContarLetras {
+
+    public static void main(String[] args) {
+        Integer[] arrayLetter = new Integer[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < arrayLetter.length; i++) {
+            arrayLetter[i] = sc.nextInt();
+        }
+        System.out.println(contarRepetidas(arrayLetter));
+
+    }
+
+    public static String contarRepetidas(Integer[] arrayLetter) {
+
+        if (arrayLetter == null)
+            return null;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Integer Letter : arrayLetter) {
+            if (map.containsKey(Letter)) {
+                Integer value = map.get(Letter);
+                value++;
+                map.replace(Letter, value);
+            } else {
+                map.put(Letter, 1);
+            }
+        }
+        return map.toString();
 
     }
 }
